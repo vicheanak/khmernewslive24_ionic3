5061749d134879fddfd7d95a1603a87a905ccd72
Vicheanak Vannavy <den@Vicheanaks-MacBook-Pro.local>
2018-10-19 05:45:38 +0700

    v0.0.1

---
+++ src/app/app.module.ts
@@ -0,0 +1,66 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule, Injectable, Injector } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { SocialSharing } from '@ionic-native/social-sharing';
+
+import { Pro } from '@ionic/pro';
+
+
+Pro.init('4dc427d8', {
+  appVersion: '0.0.1'
+})
+
+@Injectable()
+export class MyErrorHandler implements ErrorHandler {
+  ionicErrorHandler: IonicErrorHandler;
+
+  constructor(injector: Injector) {
+    try {
+      this.ionicErrorHandler = injector.get(IonicErrorHandler);
+    } catch(e) {
+      // Unable to get the IonicErrorHandler provider, ensure
+      // IonicErrorHandler has been added to the providers list below
+    }
+  }
+
+  handleError(err: any): void {
+    Pro.monitoring.handleNewError(err);
+    // Remove this if you want to disable Ionic's auto exception handling
+    // in development mode.
+    this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+  }
+}
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    SocialSharing,
+    IonicErrorHandler,
+    {provide: ErrorHandler, useClass: MyErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,101 @@
+import { Component } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import { SocialSharing } from '@ionic-native/social-sharing';
+
+import { Pro } from '@ionic/pro';
+
+
+@Component({
+	selector: 'page-home',
+	templateUrl: 'home.html'
+})
+export class HomePage {
+
+
+	public deployChannel = "";
+	public isBeta = false;
+	public downloadProgress = 0;
+
+
+	constructor(private platform: Platform, private socialSharing: SocialSharing, public navCtrl: NavController) {
+
+	}
+
+
+	async checkChannel() {
+		try {
+			const res = await Pro.deploy.getConfiguration();
+			this.deployChannel = res.channel;
+			this.isBeta = (this.deployChannel === 'Beta')
+		} catch (err) {
+			// We encountered an error.
+			// Here's how we would log it to Ionic Pro Monitoring while also catching:
+
+			// Pro.monitoring.exception(err);
+		}
+	}
+
+	async toggleBeta() {
+		const config = {
+			channel: (this.isBeta ? 'Beta' : 'Production')
+		}
+
+		try {
+			await Pro.deploy.configure(config);
+			await this.checkChannel();
+			await Pro.deploy.sync({updateMethod: 'auto'}); // Alternatively, to customize how this works, use performManualUpdate()
+		} catch (err) {
+			// We encountered an error.
+			// Here's how we would log it to Ionic Pro Monitoring while also catching:
+
+			// Pro.monitoring.exception(err);
+		}
+
+	}
+
+	async performManualUpdate() {
+
+    /*
+      Here we are going through each manual step of the update process:
+      Check, Download, Extract, and Redirect.
+
+      Ex: Check, Download, Extract when a user logs into your app,
+        but Redirect when they logout for an app that is always running
+        but used with multiple users (like at a doctors office).
+        */
+
+        try {
+        	const update = await Pro.deploy.checkForUpdate();
+
+        	if (update.available){
+        		this.downloadProgress = 0;
+
+        		await Pro.deploy.downloadUpdate((progress) => {
+        			this.downloadProgress = progress;
+        		})
+        		await Pro.deploy.extractUpdate();
+        		await Pro.deploy.reloadApp();
+        	}
+        } catch (err) {
+        	// We encountered an error.
+        	// Here's how we would log it to Ionic Pro Monitoring while also catching:
+
+        	// Pro.monitoring.exception(err);
+        }
+
+    }
+
+    shareOnFacebook() {
+    	let appName = 'facebook';
+    	if (this.platform.is('ios')) {
+    		appName = 'com.apple.social.facebook'
+    	}
+
+    	this.socialSharing.shareViaFacebook('Test', null, "www.google.com").then(() => {
+
+    	}).catch((msg) => {
+
+    	});
+    }
+
+}
